4439d5a838999ea79852560eef71f439a9d3f45e
csking2 <csking2@loyola.edu>
2024-09-28 13:02:59 -0400

    revision for lab 2

--- main.py
+++ main.py
@@ -8,33 +8,28 @@
 # Credits: This code is based on the example we were given in the excel spreadsheet
 from math import ceil
 
-#this program solves for the future population of country and sees if it will increase or decrease over time
+#the purpose of this program solves for the future population of country and sees if it will increase or decrease over time
 
 #prompt user to input seconds between birth
-sec_birth_str = float(input('enter seconds between birth:'))
-sec_birth_int = int(sec_birth_str)
+sec_birth = float(input('Enter seconds between birth:'))
 
 #prompt user to input seconds between death
-sec_death_str = float(input('enter seconds between death:'))
-sec_death_int = int(sec_death_str)
+sec_death = float(input('Enter seconds between death:'))
 
 #prompt user to input immigration
-sec_immigration_str = float(input('enter seconds between immigrant:'))
-sec_immigration_int = int(sec_immigration_str)
+sec_immigration = float(input('Enter seconds between immigrant:'))
 
 #prompt user to input current population of the country
-current_population_str = float(input('enter current population:'))
-current_population_int = int(current_population_str)
+current_population = float(input('Enter current population:'))
 
 #prompt user to input years in the future
-years_str = float(input('enter years:'))
-years_int = int(years_str)
+years = float(input('Enter years in the future:'))
 
 #calculate the change in population
-change_in_population_int: float = (((31536000 / sec_birth_int) + (31536000 / sec_immigration_int)-(31536000 / sec_death_int)) * years_int)
+change_in_population = (((31536000 / sec_birth) + (31536000 / sec_immigration)-(31536000 / sec_death)) * years)
 
 #calculate the new population
-New_population = current_population_int + change_in_population_int
+New_population = current_population + change_in_population
 
 #this rounds up to the nearest integer
 New_population = ceil(New_population)
@@ -43,7 +38,7 @@ New_population = ceil(New_population)
 print('Your population will be :', New_population)
 
 #calculate if the population increased or decreased
-if current_population_int < New_population:
+if current_population < New_population:
     print('the population increased')
 else:
     print('the population decreased')
